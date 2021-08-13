58b8ca56e990593b369e6e04159820a7de9de6e1
MarkKu7 <marksrc7@gmail.com>
2021-08-13 12:25:11 -0700

    added if branching statement and a no name alert

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
-$(document).ready(function() {
-  $("#survey").submit(function(event) {
-    event.preventDefault();
+$(document).ready(function () {
+  $("#survey").submit(function (event) {
     const name = $("#name").val();
     const Input1 = praseInt($("input:radio[name=drink]:checked").val());
     const Input2 = praseInt($("input:radio[name=movie]:checked").val());
@@ -8,5 +7,29 @@ $(document).ready(function() {
     const Input4 = praseInt($("input:radio[name=pet]:checked").val());
     const Input5 = praseInt($("input:radio[name=school]:checked").val());
 
-  });
+    const total = Input1 + Input2 + Input3 + Input4 + Input5
+
+    event.preventDefault();
+    $("#inputName").text(name);
+
+    if (!name) {
+      $("#noname").show();
+    } else if (total >= 11) {
+      $("#result").show();
+      $("#javascript").show();
+      $("#python").hide();
+      $("#csharp").hide();
+    } else if (total > 7 && total < 10) {
+      $("#result").show();
+      $("#javascript").hide();
+      $("#python").show();
+      $("#csharp").hide();
+    } else {
+      $("#result").show();
+      $("#javascript").hide();
+      $("#python").hide();
+      $("#csharp").show();
+    }
+
+    });
 });
