0bbcb7d0b6c6e39218aa859f3d606bca2f5ad42c
MarkKu7 <marksrc7@gmail.com>
2021-08-13 13:32:33 -0700

    fixed spelling error praseInt to parseInt

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
   $("#survey").submit(function (event) {
     const name = $("#name").val();
-    const Input1 = praseInt($("input:radio[name=drink]:checked").val());
-    const Input2 = praseInt($("input:radio[name=movie]:checked").val());
-    const Input3 = praseInt($("input:radio[name=activity]:checked").val());
-    const Input4 = praseInt($("input:radio[name=pet]:checked").val());
-    const Input5 = praseInt($("input:radio[name=school]:checked").val());
+    const Input1 = parseInt($("input:radio[name=drink]:checked").val());
+    const Input2 = parseInt($("input:radio[name=movie]:checked").val());
+    const Input3 = parseInt($("input:radio[name=activity]:checked").val());
+    const Input4 = parseInt($("input:radio[name=pet]:checked").val());
+    const Input5 = parseInt($("input:radio[name=school]:checked").val());
 
     const total = Input1 + Input2 + Input3 + Input4 + Input5
 
